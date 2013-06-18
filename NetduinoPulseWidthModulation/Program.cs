4ce10df423482b022249ee87b6c65193fe562da7
Scott Golightly <scott_golightly@hotmail.com>
2013-06-17 23:30:40 -0600

    Seems to work
    This is the final version of the sample from Getting Started with
    Netduino updated for the 4.2 framework.
    The bulb seems dim on all but the highest settings so I probably need to
    update the offset and scale or multiply the duty cycle number by some
    scaling factor.

---
+++ NetduinoPulseWidthModulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace NetduinoPulseWidthModulation
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+            AnalogInput pot = new AnalogInput(Cpu.AnalogChannel.ANALOG_0);
+            PWM pwm = new PWM(PWMChannels.PWM_PIN_D5, 1000.0, 0.1, false);
+            pot.Offset = 0;
+            pot.Scale = 100;
+
+            double potValue = 0.0;
+
+
+            while (true)
+            {
+                potValue = pot.Read();
+                pwm.DutyCycle = potValue;
+                pwm.Start();
+
+                Thread.Sleep(10);
+            }
+        }
+
+    }
+}
